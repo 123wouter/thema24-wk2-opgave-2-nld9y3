fb94a29d1e59fec6dc799f9c791c5d84473b3eea
123wouter <wouter.wouter@hotmail.com>
2019-05-19 20:13:53 +0200

    Fixed project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,44 @@ export class AppComponent {
   message = 'Inklokken';
   currentSchedule = standaardTekst
 
-  // CODE HIER
+  clockIn (event) {
+    if (event.shiftKey) {
+      this.message = 'Ingeklokt als docent';
+    } else {
+      this.message = 'Ingeklokt als student';
+    }
+  }
+
+  clockOut () {
+    this.message = 'Fijne dag nog.';
+  }
+
+  rooster (dag) {
+    switch (dag) {
+      case 0:
+        dag = 'Maandag';
+        break;
+
+      case 1:
+        dag = 'Dinsdag';
+        break;
+
+      case 2:
+       dag = 'Woensdag';
+       break;
+
+      case 3:
+        dag = 'Donderdag';
+        break;
+
+      case 4:
+        dag = 'Vrijdag';
+        break;
+    }
+    this.currentSchedule = dag + " " + scheduleMap[dag];
+  }
+
+  roosterReset () {
+    this.currentSchedule = standaardTekst
+  }
 }
